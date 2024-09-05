980548a0b07f31bfd504d026fc1eb4679814d02c
jogisaikumar <78369747+kumaravel-altran@users.noreply.github.com>
2024-09-05 19:49:55 +0530

    Added footer and side nav updated with movie geners

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -2,27 +2,30 @@ import "./Home.css";
 
 
 function Home() {
-    const genreOfGames = [
-        'Shooting', 'MultiPlayer', 'Combat', 'Brain Storm',
-        'Driving', 'Family', 'Farm and Forest','Horror', 
+    const genreOfMovies = [
+        'Action', 'Sci-Fi', 'Fantasy','Love/Romantic', 'Comedy',
+        'Sports', 'Family-Drama', 'Adventure','Horror', 'Thriller', 'English','Tamil', 
+        'Telugu', 'Kannada', 'Korean', 'Hindi'
     ]
     return (
         <>
             <header>
                 <nav>
                 <img src="/vite.svg" alt="vite-logo" />
-                <h4>Games</h4>
-                <button>Mode</button>
+                <h4>Movies</h4>
+                <button className="btn-mode">Mode</button>
                 </nav>
 
             </header>
             <aside id="Side-nav">
                <ul id="list-items">
-                {genreOfGames.map((game, index) => {
-                    return <li key={index}>{game}</li>
+                <h3>Geners</h3>
+                {genreOfMovies.map((movie, index) => {
+                    return <li key={index}>{movie}</li>
                 })}
                </ul>
             </aside>
+            <footer> <p>© 2024 My Website. All rights reserved.</p> </footer>
         </>
     )
 }

13560654de4f8bc908ab8df95f7d224eb9b8cae0
jogisaikumar <78369747+kumaravel-altran@users.noreply.github.com>
2024-09-16 15:51:01 +0530

    Added Dark mode to the Home Page

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,33 +1,56 @@
 import "./Home.css";
-
+import { useState } from "react";
 
 function Home() {
-    const genreOfMovies = [
-        'Action', 'Sci-Fi', 'Fantasy','Love/Romantic', 'Comedy',
-        'Sports', 'Family-Drama', 'Adventure','Horror', 'Thriller', 'English','Tamil', 
-        'Telugu', 'Kannada', 'Korean', 'Hindi'
-    ]
-    return (
-        <>
-            <header>
-                <nav>
-                <img src="/vite.svg" alt="vite-logo" />
-                <h4>Movies</h4>
-                <button className="btn-mode">Mode</button>
-                </nav>
+  const genreOfMovies = [
+    "Action",
+    "Sci-Fi",
+    "Fantasy",
+    "Love/Romantic",
+    "Comedy",
+    "Sports",
+    "Family-Drama",
+    "Adventure",
+    "Horror",
+    "Thriller",
+    "English",
+    "Tamil",
+    "Telugu",
+    "Kannada",
+    "Korean",
+    "Hindi",
+  ];
 
-            </header>
-            <aside id="Side-nav">
-               <ul id="list-items">
-                <h3>Geners</h3>
-                {genreOfMovies.map((movie, index) => {
-                    return <li key={index}>{movie}</li>
-                })}
-               </ul>
-            </aside>
-            <footer> <p>© 2024 My Website. All rights reserved.</p> </footer>
-        </>
-    )
+  const [modeIsDark, setModeIsDark] = useState(false);
+
+  function HandleDarkMode() {
+    setModeIsDark(!modeIsDark);
+  }
+  return (
+    <div className={modeIsDark ? "dark-mode" : "light-mode"}>
+      <header>
+        <nav>
+          <img src="/vite.svg" alt="vite-logo" />
+          <h4>Movies</h4>
+          <button className="btn-mode" onClick={HandleDarkMode}>
+            Mode
+          </button>
+        </nav>
+      </header>
+      <aside id="Side-nav">
+        <ul id="list-items">
+          <h3>Geners</h3>
+          {genreOfMovies.map((movie, index) => {
+            return <li key={index}>{movie}</li>;
+          })}
+        </ul>
+      </aside>
+      <footer>
+        {" "}
+        <p>© 2024 My Website. All rights reserved.</p>{" "}
+      </footer>
+    </div>
+  );
 }
 
 export default Home;
